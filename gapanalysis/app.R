37b7bb455361b13d01b738b39830a07873d28269
Amanda Devine <devine.amanda.m@gmail.com>
2018-02-23 18:46:48 -0500

    Implemented rudimentary gap analysis for GGBN and GenBank

--- gapanalysis/app.R
+++ gapanalysis/app.R
@@ -7,13 +7,16 @@ library(tidyr)
 
 # Load data from Dropbox
 token <- readRDS('dropbox_token.rds')
-col <- drop_read_csv('shiny/col.csv', dtoken = token, stringsAsFactors = FALSE)
-ggbn <- drop_read_csv('shiny/ggbn.csv', dtoken = token, stringsAsFactors = FALSE)
-genbank <- drop_read_csv('shiny/genbank.csv', dtoken = token, stringsAsFactors = FALSE)
 
-get.tax.names <- function(df) {
-    
-}
+gbif <- drop_read_csv('shiny/gbif.csv', dtoken = token, stringsAsFactors = FALSE)
+
+ggbn <- drop_read_csv('shiny/ggbn.csv', dtoken = token, stringsAsFactors = FALSE)
+ggbn <- ggbn[, -1]
+ggbn$rank <- tolower(ggbn$rank)
+
+genbank <- drop_read_csv('shiny/genbank.csv', dtoken = token, stringsAsFactors = FALSE)
+genbank <- genbank[, -1]
+genbank$rank <- tolower(genbank$rank)
 
 # User interface
 ui <- fluidPage(
@@ -23,10 +26,10 @@ ui <- fluidPage(
         sidebarPanel(
             # numericInput('inp.test', 'Test Numeric Input', 20, min = 10, max = 30),
             h4('Upload Names List'),
-            fileInput('upl.names', 'Names to analyze'),
+            fileInput('inp.name.file', 'Names to analyze'),
             textAreaInput(
-                'inp.names', 
-                'or Paste list of names here',
+                'inp.name.list', 
+                'or paste list of names here (one per row)',
                 placeholder = 'Paste one name per row',
                 width = '200px',
                 height = '100px'
@@ -37,7 +40,12 @@ ui <- fluidPage(
                 'Select taxonomic level', 
                 c(
                     'All',
-                    unique(col$taxRank)
+                    'Kingdom' = 'kingdom',
+                    "Phylum/Division" = 'phylum',
+                    'Class' = 'class',
+                    'Order' = 'order',
+                    'Family' = 'family',
+                    'Genus' = 'genus'
                 ),
                 width = '200px'
             ),
@@ -46,7 +54,7 @@ ui <- fluidPage(
                 'Select kingdom', 
                 c(
                     'All',
-                    unique(col$kingdom)
+                    sort(unique(gbif$kingdom))
                 ),
                 width = '200px'
             )
@@ -66,39 +74,73 @@ ui <- fluidPage(
 
 server <- function(input, output) {
     
-    filtered.data <- reactive({
+    filtered <- reactive({
+        print(c('Input tax level:', input$inp.taxlevel))
+        print(c('Input kingdom:', input$inp.kingdom))
         if (input$inp.taxlevel == 'All' && input$inp.kingdom == 'All') {
-            c <- col
+            f <- gbif
+            n <- ggbn
+            k <- genbank
         } else if (input$inp.taxlevel == 'All' && input$inp.kingdom != 'All') {
-            c <- filter(col, kingdom == input$inp.kingdom)
+            f <- filter(gbif, kingdom == input$inp.kingdom)
+            n <- filter(ggbn, kingdom == input$inp.kingdom)
+            k <- filter(genbank, kingdom == input$inp.kingdom)
         } else if (input$inp.taxlevel != 'All' && input$inp.kingdom == 'All') {
-            c <- filter(col, taxRank == input$inp.taxlevel)
+            f <- filter(gbif, rank == input$inp.taxlevel) 
+            n <- filter(ggbn, rank == input$inp.taxlevel) 
+            k <- filter(genbank, rank == input$inp.taxlevel)
         } else {
-            c <- filter(col, taxRank == input$inp.taxlevel, kingdom == input$inp.kingdom)
+            f <- filter(gbif, rank == input$inp.taxlevel, kingdom == input$inp.kingdom)
+            n <- filter(ggbn, rank == input$inp.taxlevel, kingdom == input$inp.kingdom)
+            k <- filter(genbank, rank == input$inp.taxlevel, kingdom == input$inp.kingdom)
         }
-        c %>% select(kingdom, phylum, classis, ordo, family, genus) %>% print()
+        list(gbif = f, ggbn = n, genbank = k)
+    })
+    
+    query.names <- reactive({
+        if ( is.null(input$inp.name.file) && is.null(input$inp.name.list) ) {
+            return(NULL)
+        } else if ( input$inp.name.list == 'plant families' ) {
+            return(c('Asteraceae', 'Betulaceae', 'Caprifoliaceae', 'Droseraceae', 
+                     'Ericaceae', 'Fabaceae', 'Gesneriaceae','Heliconiaceae',
+                     'Iridaceae', 'Jungermanniaceae', 'Kirkiaceae', 'Lauraceae',
+                     'Melastomataceae', 'Nyctaginaceae', 'Orchidaceae', 'Poaceae',
+                     'Quillajaceae', 'Rosaceae', 'Sapindaceae', 'Taxaceae',
+                     'Urticaceae', 'Violaceae', 'Woodsiaceae', 'Xanthorrhoeaceae',
+                     'Zingiberaceae'))
+        } else if ( is.null(input$inp.name.file) && !is.null(input$inp.name.list) ) {
+            return(strsplit(input$inp.name.list, "\n"))
+        }
     })
     
     output$venn.diagram <- renderPlot({
 
-        coldata <- filtered.data() %>% 
-            gather(key = rnk, value = txnm, kingdom, phylum, classis, ordo, family, genus) %>%
-            filter(txnm != '') %>%
-            group_by(rnk) %>%
-            summarise(n.names = n_distinct(txnm))
-
-        # if (input$toggle.graph %% 2 == 0) {
-        # barplot(coldata$txnm)
-        # } else {
-        pie(coldata$n.names, labels = coldata$rnk)
-        # }
+        # coldata <- filtered.col() %>% 
+        #     gather(key = rnk, value = txnm, kingdom, phylum, classis, ordo, family, genus) %>%
+        #     filter(txnm != '') %>%
+        #     group_by(rnk) %>%
+        #     summarise(n.names = n_distinct(txnm))
+        # 
+        # # if (input$toggle.graph %% 2 == 0) {
+        # # barplot(coldata$txnm)
+        # # } else {
+        # pie(coldata$n.names, labels = coldata$rnk)
+        # # }
+        return()
 
     })
     
     output$results.table <- renderTable({
-        coltable <- filtered.data()
-        colnames(coltable) <- c('Kingdom', 'Phylum/Division', 'Class', 'Order', 'Family', 'Genus')
-        head(coltable, 10)
+        if (is.null(query.names())) {
+            return()
+        }
+        query.df <- data.frame(query.names())
+        colnames(query.df) <- c('qn')
+        
+        query.df$ggbn <- query.df$qn %in% filtered()$ggbn$name
+        query.df$genbank <- query.df$qn %in% filtered()$genbank$name
+        colnames(query.df) <- c('Query Name', 'In GGBN', 'In GenBank')
+        query.df
     })
 
 }
